40bf25858676311c3e902dd5e564da5368d2a6b1
dmitry <rbdeman@yandex.ru>
2024-03-09 22:53:54 +0300

    готово 7

--- myproject/settings.py
+++ myproject/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import os.path
+
+import os
+# import os.path
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -26,10 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-if os.environ.get("DEBUG") == "False":
-    DEBUG = False
-else:
-    DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 SESSION_COOKIE_SECURE = True

aac1c0d27c792e64c1dc84c452aef2b573344a37
dmitry <rbdeman@yandex.ru>
2024-03-11 21:36:04 +0300

    изменил настройки email

--- myproject/settings.py
+++ myproject/settings.py
@@ -84,8 +84,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'test@gmail.com'
-EMAIL_HOST_PASSWORD = '123'
+EMAIL_HOST_USER = 'dmitryrogovtsov14@gmail.com'
+EMAIL_HOST_PASSWORD = 'Kon21071979'
+DEFAULT_FROM_EMAIL = 'dmitryrogovtsov14@gmail.com'
 
 
 # Database
